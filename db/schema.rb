8781f777bfcea6ffee1c13e8bcc8df948ef4b7f2
maiya-22 <maiya_o@yahoo.com>
2018-05-23 19:12:57 -0400

    stylesheet for tasks linked

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-# <<<<<<< HEAD
-# ActiveRecord::Schema.define(version: 20180522173247) do
-# =======
-ActiveRecord::Schema.define(version: 20180522031502) do
-# >>>>>>> master
+ActiveRecord::Schema.define(version: 20180522173247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
